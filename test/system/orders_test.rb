6d2f8293c13aeaf6e445964c5c408657cf20519c
Pierre Lasante <pierre.lasante@videotron.ca>
2020-04-10 06:14:34 -0700

    Using Capybara for system test

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -6,36 +6,10 @@ class OrdersTest < ApplicationSystemTestCase
   end
 
   test "visiting the index" do
+    # visit localhost:3000/orders
     visit orders_url
+    # find <h1>Orders</h1> in the page
     assert_selector "h1", text: "Orders"
-  end
-
-  test "creating a Order" do
-    visit orders_url
-    click_on "New Order"
-
-    fill_in "Address", with: @order.address
-    fill_in "Email", with: @order.email
-    fill_in "Name", with: @order.name
-    fill_in "Pay type", with: @order.pay_type
-    click_on "Create Order"
-
-    assert_text "Order was successfully created"
-    click_on "Back"
-  end
-
-  test "updating a Order" do
-    visit orders_url
-    click_on "Edit", match: :first
-
-    fill_in "Address", with: @order.address
-    fill_in "Email", with: @order.email
-    fill_in "Name", with: @order.name
-    fill_in "Pay type", with: @order.pay_type
-    click_on "Update Order"
-
-    assert_text "Order was successfully updated"
-    click_on "Back"
   end
 
   test "destroying a Order" do
@@ -45,5 +19,41 @@ class OrdersTest < ApplicationSystemTestCase
     end
 
     assert_text "Order was successfully destroyed"
+  end
+
+  test "check routing number" do
+    visit store_index_url
+
+    click_on "Add to Cart", match: :first
+
+    click_on 'Checkout'
+
+    fill_in 'order_name', with: 'Pierre'
+    fill_in 'order_address', with: '2592 ch Ste-Marie'
+    fill_in 'order_email', with: 'pierre@email.com'
+
+    assert_no_selector "#order_routing_number"
+
+    select 'Check', from: 'Pay type'
+
+    assert_selector "#order_routing_number"
+  end
+
+  test "check po number" do
+    visit store_index_url
+
+    click_on "Add to Cart", match: :first
+
+    click_on 'Checkout'
+
+    fill_in 'order_name', with: 'Pierre'
+    fill_in 'order_address', with: '2592 ch Ste-Marie'
+    fill_in 'order_email', with: 'pierre@email.com'
+
+    assert_no_selector "#order_po_number"
+
+    select 'Purchase order', from: 'Pay type'
+
+    assert_selector "#order_po_number"
   end
 end
