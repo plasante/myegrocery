8f91542cca1ecc33f908f1e6ae177a842fbad36e
Pierre Lasante <pierre.lasante@videotron.ca>
2020-04-10 04:35:39 -0700

    Remove a useless test.

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -46,10 +46,4 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to orders_url
   end
-
-  test "requires item in cart" do
-    get new_order_url
-    assert_redirected_to store_index_path
-    assert_equal flash[:notice], 'Your cart is empty'
-  end
 end
